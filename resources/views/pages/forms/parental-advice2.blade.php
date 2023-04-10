dadcb92f4e10c8151551511443d614fa062924d2
badsektor <gerald.villaran@lspu.edu.ph>
2023-04-10 21:31:26 +0800

    brea(d) done with form 2

--- resources/views/pages/forms/parental-advice2.blade.php
+++ resources/views/pages/forms/parental-advice2.blade.php
@@ -6,7 +6,7 @@
                     <form id="form-process-form" method="POST" action="{{ route('process-parental-advice2') }}" class="w-100">
                         @csrf
                         <div class="w-100 d-flex align-items-center">
-                            <h3 class="card-title fw-bold text-nowrap">Parental Advice (2)</h3><input class="form-control h-30px fw-normal d-inline-block" type="text" id="form_id" name="form_id" style="background-color: #F2F0F0; width: 60px; margin-left: 4px; margin-right: 4px" disabled>
+                            <h3 class="card-title fw-bold text-nowrap">Parental Advice (2)</h3><input class="form-control h-30px fw-normal d-inline-block" type="text" id="form_id" name="form_id" style="background-color: #F2F0F0; width: 60px; margin-left: 4px; margin-right: 4px" readonly>
                             <a href="#" id="btn_clear" class="btn btn-sm btn-secondary d-flex justify-content-center align-items-center" style="font-size: 11px; height: 30px; width: 50px;">Clear</a>
                             <div class="w-100 card-toolbar justify-content-end">
                                 <button type="button" class="btn btn-sm btn-info" id="btn_clear" data-bs-toggle="modal" data-bs-target="#form2_modal">
@@ -93,7 +93,7 @@
                                             </div>
                                             <div class="d-flex gap-2 align-items-center">
                                                 <div class="text-nowrap">To</div>
-                                                <input class="form-control h-30px fw-normal" type="text" name="name_female2" style="background-color: #F2F0F0; width: 180px">
+                                                <input class="form-control h-30px fw-normal" type="text" id="name_female2" name="name_female2" style="background-color: #F2F0F0; width: 180px">
                                             </div>
                                         </div>
                                     </td>
@@ -101,7 +101,7 @@
                                 <tr>
                                     <td colspan="20">
                                         <div class="px-5 py-5">
-                                            <p>Our/My advice upon your intended marriage with <input class="form-control h-30px fw-normal d-inline-block" type="text" name="name_male2" style="background-color: #F2F0F0; width: 180px; margin-left: 4px; margin-right: 4px"> having been asked by you, and knowing no legal impediment to this marriage, we/I hereby advice you to marry her.</p>
+                                            <p>Our/My advice upon your intended marriage with <input class="form-control h-30px fw-normal d-inline-block" type="text" id="name_male2" name="name_male2" style="background-color: #F2F0F0; width: 180px; margin-left: 4px; margin-right: 4px"> having been asked by you, and knowing no legal impediment to this marriage, we/I hereby advice you to marry her.</p>
                                         </div>
                                     </td>
                                 </tr>
@@ -142,8 +142,13 @@
                         </table>
                         <div class="row mt-3">
                             <div class="col-md-6">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Save') }}
+                                <button type="button" class="btn btn-primary me-10" id="btn_process_form">
+                                    <span class="indicator-label"><i class="fa fa-save"></i>
+                                        Save
+                                    </span>
+                                    <span class="indicator-progress">
+                                        Please wait... <span class="spinner-border spinner-border-sm align-middle ms-2"></span>
+                                    </span>
                                 </button>
                             </div>
                         </div>
@@ -190,10 +195,10 @@
             "createdRow": function(row, data, dataIndex) {
                 console.log("Data used to create row:", data);
                 $(row).attr('data-id', data.id);
-                $(row).attr('data-data-address', data.municipality);
-                $(row).attr('data-address2', data.province);
-                $(row).attr('data-name-male', data.name);
-                $(row).attr('data-name-female', data.address);
+                $(row).attr('data-address', data.address);
+                $(row).attr('data-address2', data.address2);
+                $(row).attr('data-name-male', data.name_male);
+                $(row).attr('data-name-female', data.name_female);
                 $(row).attr('data-created', data.created_at);
                 $(row).attr('data-updated', data.updated_at);
                 $(row).addClass('selectable');
@@ -211,61 +216,109 @@
 
         $('#btn_process_form').on('click', function(event) {
 
-        var form = $('#form-process-form');
-        var url = form.attr('action');
-        var method = form.attr('method');
-        var data = form.serialize();
-        var form_id = $('#form_id').val();
-        console.log(data);
-        // Check if owner_id field is present
-        if (data.indexOf('form_id=') !== -1) {
-            console.log('Update!');
-        } else {
-            console.log('New!');
-        }
+            var form = $('#form-process-form');
+            var url = form.attr('action');
+            var method = form.attr('method');
+            var data = form.serialize();
+            var form_id = $('#form_id').val();
+            console.log(data);
 
-        $.ajax({
-            url: '/parental-form2/process',
-            method: method,
-            data: data,
-            dataType: 'json',
-            headers: {
-                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-            },
-            beforeSend: function() {
-                // Show loading indicator
-                $('#btn_process_form .indicator-label').hide();
-                $('#btn_process_form .indicator-progress').show();
-            },
-            success: function(response) {
-                // Hide loading indicator
-                $('#btn_process_form .indicator-label').show();
-                $('#btn_process_form .indicator-progress').hide();
+            // Extract the form_id value from the data variable
+            var form_id_value = data.split('&').find(function(param) {
+                return param.startsWith('form_id=');
+            });
 
-                // Show success message in SweetAlert
-                Swal.fire({
-                    title: 'Success',
-                    text: response.message,
-                    icon: 'success',
-                    showConfirmButton: false,
-                    timer: 2000
-                }).then(() => {
-                    refreshDataTable();
-                });
-            },
-            error: function(response) {
-                // Hide loading indicator
-                $('#btn_process_form .indicator-label').show();
-                $('#btn_process_form .indicator-progress').hide();
-
-                // Show error message in SweetAlert
-                Swal.fire({
-                    title: 'Error',
-                    text: response.responseJSON.message,
-                    icon: 'error',
-                    confirmButtonText: 'OK'
-                });
+            if (form_id_value) {
+                form_id_value = form_id_value.split('=')[1];
+                console.log("Form ID value from data:", form_id_value);
+            } else {
+                console.log("Form ID not found in data");
             }
+
+            // Check if owner_id field is present
+            if (data.indexOf('form_id=') !== -1) {
+                console.log('Update!');
+            } else {
+                console.log('New!');
+            }
+
+            $.ajax({
+                url: '/parental-form2/process',
+                method: method,
+                data: data,
+                dataType: 'json',
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                },
+                beforeSend: function() {
+                    // Show loading indicator
+                    $('#btn_process_form .indicator-label').hide();
+                    $('#btn_process_form .indicator-progress').show();
+                },
+                success: function(response) {
+                    // Hide loading indicator
+                    $('#btn_process_form .indicator-label').show();
+                    $('#btn_process_form .indicator-progress').hide();
+
+                    // Show success message in SweetAlert
+                    Swal.fire({
+                        title: 'Success',
+                        text: response.message,
+                        icon: 'success',
+                        showConfirmButton: false,
+                        timer: 2000
+                    }).then(() => {
+                        refreshDataTable();
+                    });
+                },
+                error: function(response) {
+                    // Hide loading indicator
+                    $('#btn_process_form .indicator-label').show();
+                    $('#btn_process_form .indicator-progress').hide();
+
+                    // Show error message in SweetAlert
+                    Swal.fire({
+                        title: 'Error',
+                        text: response.responseJSON.message,
+                        icon: 'error',
+                        confirmButtonText: 'OK'
+                    });
+                }
+            });
+        });
+
+        jQuery(function() {
+            $('#parental-form-table').on('click', 'tr.selectable', function() {
+                let form_id = $(this).data('id');
+                let name_male = $(this).data('name-male');
+                let name_female = $(this).data('name-female');
+                let address = $(this).data('address');
+                let address2 = $(this).data('address2');
+                const created_at = new Date($(this).data('created-at')).toLocaleString();
+                const updated_at = new Date($(this).data('updated-at')).toLocaleString();
+                // do something with the selected row ID
+                console.log(form_id);
+                $('#form_id').val(form_id);
+                $('#name_male').val(name_male);
+                $('#name_male2').val(name_male);
+                $('#name_female').val(name_female);
+                $('#name_female2').val(name_female);
+                $('#address').val(address);
+                $('#address2').val(address2);
+                const modal = bootstrap.Modal.getInstance($('#form2_modal'));
+                modal.hide();
+            });
+
+            $('#btn_clear').on('click', function(event) {
+                event.preventDefault(); // prevent the default behavior of the link
+                $('#form_id').val("");
+                $('#name_male').val("");
+                $('#name_female').val("");
+                $('#address').val("");
+                $('#address2').val("");
+                // $('#owner_created_at').text("---");
+                // $('#owner_updated_at').text("---");
+            });
         });
     });
 
--- routes/web.php
+++ routes/web.php
@@ -69,62 +69,64 @@ Route::middleware('auth')->group(function () {
 
 Route::resource('users', UsersController::class);
 
-//Actual Use
-Route::get('/actual-uses', [ActualUseController::class, 'index'])->name('actual-uses');
-Route::get('/actual-use/create', [ActualUseController::class, 'create'])->name('create-actual-use');
-Route::post('/actual-use/store', [ActualUseController::class, 'store'])->name('store-actual-use');
-Route::get('/actual-use/show/{actual_use}', [ActualUseController::class, 'show'])->name('show-actual-use');
-Route::get('/actual-use/edit/{actual_use}', [ActualUseController::class, 'edit'])->name('edit-actual-use');
-Route::put('/actual-use/update/{actual_use}', [ActualUseController::class, 'update'])->name('update-actual-use');
-Route::delete('/actual-use/destroy/{actual_use}', [ActualUseController::class, 'destroy'])->name('destroy-actual-use');
-Route::post('/actual-uses/batch-destroy', [ActualUseController::class, 'batchDestroy'])->name('batch-destroy-actual-uses');
+Route::middleware(['auth'])->group(function () {
+    //Actual Use
+    Route::get('/actual-uses', [ActualUseController::class, 'index'])->name('actual-uses');
+    Route::get('/actual-use/create', [ActualUseController::class, 'create'])->name('create-actual-use');
+    Route::post('/actual-use/store', [ActualUseController::class, 'store'])->name('store-actual-use');
+    Route::get('/actual-use/show/{actual_use}', [ActualUseController::class, 'show'])->name('show-actual-use');
+    Route::get('/actual-use/edit/{actual_use}', [ActualUseController::class, 'edit'])->name('edit-actual-use');
+    Route::put('/actual-use/update/{actual_use}', [ActualUseController::class, 'update'])->name('update-actual-use');
+    Route::delete('/actual-use/destroy/{actual_use}', [ActualUseController::class, 'destroy'])->name('destroy-actual-use');
+    Route::post('/actual-uses/batch-destroy', [ActualUseController::class, 'batchDestroy'])->name('batch-destroy-actual-uses');
 
-//Additional Item
-Route::get('/items', [AdditionalItemController::class, 'index'])->name('items');
-Route::get('/item/create', [AdditionalItemController::class, 'create'])->name('create-item');
-Route::post('item/store', [AdditionalItemController::class, 'store'])->name('store-item');
-Route::get('/item/show/{item}', [AdditionalItemController::class, 'show'])->name('show-item');
-Route::get('/item/edit/{item}', [AdditionalItemController::class, 'edit'])->name('edit-item');
-Route::put('/item/update/{item}', [AdditionalItemController::class, 'update'])->name('update-item');
-Route::delete('/item/destroy/{item}', [AdditionalItemController::class, 'destroy'])->name('destroy-item');
-Route::post('/item/batch-destroy', [AdditionalItemController::class, 'batchDestroy'])->name('batch-destroy-items');
+    //Additional Item
+    Route::get('/items', [AdditionalItemController::class, 'index'])->name('items');
+    Route::get('/item/create', [AdditionalItemController::class, 'create'])->name('create-item');
+    Route::post('item/store', [AdditionalItemController::class, 'store'])->name('store-item');
+    Route::get('/item/show/{item}', [AdditionalItemController::class, 'show'])->name('show-item');
+    Route::get('/item/edit/{item}', [AdditionalItemController::class, 'edit'])->name('edit-item');
+    Route::put('/item/update/{item}', [AdditionalItemController::class, 'update'])->name('update-item');
+    Route::delete('/item/destroy/{item}', [AdditionalItemController::class, 'destroy'])->name('destroy-item');
+    Route::post('/item/batch-destroy', [AdditionalItemController::class, 'batchDestroy'])->name('batch-destroy-items');
 
-//Applicant
-Route::get('/applicants', [ApplicantController::class, 'index'])->name('applicants');
-Route::get('/applicant/create', [ApplicantController::class, 'create'])->name('create-applicant');
-Route::post('/applicant/store', [ApplicantController::class, 'store'])->name('store-applicant');
-Route::get('/applicant/show/{applicant}', [ApplicantController::class, 'show'])->name('show-applicant');
-Route::get('/applicant/edit/{applicant}', [ApplicantController::class, 'edit'])->name('edit-applicant');
-Route::put('/applicant/update/{applicant}', [ApplicantController::class, 'update'])->name('update-applicant');
-Route::delete('/applicant/destroy/{applicant}', [ApplicantController::class, 'destroy'])->name('destroy-applicant');
-Route::post('/applicants/batch-destroy', [ApplicantController::class, 'batchDestroy'])->name('batch-destroy-applicants');
+    //Applicant
+    Route::get('/applicants', [ApplicantController::class, 'index'])->name('applicants');
+    Route::get('/applicant/create', [ApplicantController::class, 'create'])->name('create-applicant');
+    Route::post('/applicant/store', [ApplicantController::class, 'store'])->name('store-applicant');
+    Route::get('/applicant/show/{applicant}', [ApplicantController::class, 'show'])->name('show-applicant');
+    Route::get('/applicant/edit/{applicant}', [ApplicantController::class, 'edit'])->name('edit-applicant');
+    Route::put('/applicant/update/{applicant}', [ApplicantController::class, 'update'])->name('update-applicant');
+    Route::delete('/applicant/destroy/{applicant}', [ApplicantController::class, 'destroy'])->name('destroy-applicant');
+    Route::post('/applicants/batch-destroy', [ApplicantController::class, 'batchDestroy'])->name('batch-destroy-applicants');
 
-//Application
-Route::get('/applications', [ApplicationController::class, 'index'])->name('applications');
-Route::get('/application/create', [ApplicationController::class, 'create'])->name('create-application');
-Route::post('/application/store', [ApplicationController::class, 'store'])->name('store-application');
-Route::get('/application/show/{application}', [ApplicationController::class, 'show'])->name('show-application');
-Route::get('/application/edit/{application}', [ApplicationController::class, 'edit'])->name('edit-application');
-Route::put('/application/update/{application}', [ApplicationController::class, 'update'])->name('update-application');
-Route::delete('/application/destroy/{application}', [ApplicationController::class, 'destroy'])->name('destroy-application');
-Route::post('/applications/batch-destroy', [ApplicationController::class, 'batchDestroy'])->name('batch-destroy-applications');
+    //Application
+    Route::get('/applications', [ApplicationController::class, 'index'])->name('applications');
+    Route::get('/application/create', [ApplicationController::class, 'create'])->name('create-application');
+    Route::post('/application/store', [ApplicationController::class, 'store'])->name('store-application');
+    Route::get('/application/show/{application}', [ApplicationController::class, 'show'])->name('show-application');
+    Route::get('/application/edit/{application}', [ApplicationController::class, 'edit'])->name('edit-application');
+    Route::put('/application/update/{application}', [ApplicationController::class, 'update'])->name('update-application');
+    Route::delete('/application/destroy/{application}', [ApplicationController::class, 'destroy'])->name('destroy-application');
+    Route::post('/applications/batch-destroy', [ApplicationController::class, 'batchDestroy'])->name('batch-destroy-applications');
 
-Route::get('/application/show-form/{application}', [ApplicationController::class, 'show_form'])->name('show-application-form');
-Route::get('/table', [ApplicationController::class, 'table']);
-Route::get('/docu-auto', [ApplicationController::class, 'docu_auto']);
-Route::get('/calendar', [AppointmentController::class, 'index']);
+    Route::get('/application/show-form/{application}', [ApplicationController::class, 'show_form'])->name('show-application-form');
+    Route::get('/table', [ApplicationController::class, 'table']);
+    Route::get('/docu-auto', [ApplicationController::class, 'docu_auto']);
+    Route::get('/calendar', [AppointmentController::class, 'index']);
 
-Route::get('/forms/parental-advice', [FormsController::class, 'parentalAdvice']);
-Route::post('/parental-form/process', [ParentalForm1Controller::class, 'process'])->name('process-parental-advice');
-Route::get('/parental-forms/all', [ParentalForm1Controller::class, 'all'])->name('all.parental-forms');
+    Route::get('/forms/parental-advice', [FormsController::class, 'parentalAdvice']);
+    Route::post('/parental-form/process', [ParentalForm1Controller::class, 'process'])->name('process-parental-advice');
+    Route::get('/parental-forms/all', [ParentalForm1Controller::class, 'all'])->name('all.parental-forms');
 
-Route::get('/forms/parental-advice2', [FormsController::class, 'parentalAdvice2']);
-Route::post('/parental-form2/process', [ParentalForm2Controller::class, 'process'])->name('process-parental-advice2');
-Route::get('/parental-form2s/all', [ParentalForm2Controller::class, 'all'])->name('all.parental-form2s');
+    Route::get('/forms/parental-advice2', [FormsController::class, 'parentalAdvice2']);
+    Route::post('/parental-form2/process', [ParentalForm2Controller::class, 'process'])->name('process-parental-advice2');
+    Route::get('/parental-form2s/all', [ParentalForm2Controller::class, 'all'])->name('all.parental-form2s');
 
-Route::get('/forms/parental-consent', [FormsController::class, 'parentalConsent']);
-Route::post('/parental-form3/process', [ParentalForm3Controller::class, 'process'])->name('process-parental-advice3');
-Route::get('/parental-form3s/all', [ParentalForm3Controller::class, 'all'])->name('all.parental-form3s');
+    Route::get('/forms/parental-consent', [FormsController::class, 'parentalConsent']);
+    Route::post('/parental-form3/process', [ParentalForm3Controller::class, 'process'])->name('process-parental-advice3');
+    Route::get('/parental-form3s/all', [ParentalForm3Controller::class, 'all'])->name('all.parental-form3s');
+});
 /**
  * Socialite login using Google service
  * https://laravel.com/docs/8.x/socialite

ca3f2f2aec0d60abe0b1bb80cbda6c11a20218ef
Gerald Pogi <gerald.villaran@lspu.edu.ph>
2023-04-21 21:06:28 +0700

    sir villaran kups

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -24,28 +24,52 @@
                 <div class="card-body">
                     <h5 class="card-title">Requirements for Application for Marriage License</h5>
                     <div class="separator my-10"></div>
-                    <p class="card-text"><h6>Requirements:</h6>
-                    <p>
-                        •Birth Certificate (both Local or PSA)<br>
-                        •CENOMAR w/official receipt<br>
-                        •Valid ID (both)<br>
-                        •Brgy. Certification (both)<br>
-                        •2x2 picture (both)<br>
-                        •Pre-Marital Certificate (DSWD)<br>
-                        •Parental Consent (18-21 y.o.)<br>
-                        •Parental Advice (22-24 y.o.)<br><br>
-
-
-                        <b>FEES:</b><br>
-                        •Php 100.00 for Pre-Marital Seminar<br>
-                        •Php 200.00 for Application of Marriage License<br>
-                        •Php 300.00 for Application of Marriage<br>
-                        •Php 200.00 for Solemnization (if Civil-mayor)<br>
-                    </p>
-
+                    <div class="d-flex justify-content-between">
+                        <div>
+                            <p class="card-text"><h6>Requirements:</h6>
+                                <p>
+                                    •Birth Certificate (both Local or PSA)<br>
+                                    •CENOMAR w/official receipt<br>
+                                    •Valid ID (both)<br>
+                                    •Brgy. Certification (both)<br>
+                                    •2x2 picture (both)<br>
+                                    •Pre-Marital Certificate (DSWD)<br>
+                                    •Parental Consent (18-21 y.o.)<br>
+                                    •Parental Advice (22-24 y.o.)<br><br>
+            
+            
+                                    <b>FEES:</b><br>
+                                    •Php 100.00 for Pre-Marital Seminar<br>
+                                    •Php 200.00 for Application of Marriage License<br>
+                                    •Php 300.00 for Application of Marriage<br>
+                                    •Php 200.00 for Solemnization (if Civil-mayor)<br>
+                                </p>
+                        </div>
+                        <div class="d-flex flex-row gap-4">
+                            <div class="d-flex flex-column align-items-center jusity-content-center p-4 rounded" style="border: 1px solid black; height: min-content;">
+                                <div class="fs-1 fw-bold">0</div>
+                                <div class="">Daily Applications</div>
+                            </div>
+                            <div class="d-flex flex-column align-items-center jusity-content-center p-4 rounded" style="border: 1px solid black; height: min-content;">
+                                <div class="fs-1 fw-bold">0</div>
+                                <div class="">Weekly Applications</div>
+                            </div>
+                            <div class="d-flex flex-column align-items-center jusity-content-center p-4 rounded" style="border: 1px solid black; height: min-content;">
+                                <div class="fs-1 fw-bold">0</div>
+                                <div class="">Monthly Applications</div>
+                            </div>
+                            <div class="d-flex flex-column align-items-center jusity-content-center p-4 rounded" style="border: 1px solid black; height: min-content;">
+                                <div class="fs-1 fw-bold">0</div>
+                                <div class="">Yearly Applications</div>
+                            </div>
+                        </div>
+                    </div>
+    
                 </div>
+                
             </div>
         </div>
+        
     </div>
 
 </x-base-layout>
